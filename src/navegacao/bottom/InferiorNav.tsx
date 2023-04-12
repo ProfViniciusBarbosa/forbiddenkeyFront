e62abe37cf3e89b82eefac544d4d6116772b0f88
Vini <prof.vinicius.monteiro@gmail.com>
2023-04-11 21:55:18 -0300

    Funcionando icone de perfil

--- src/navegacao/bottom/InferiorNav.tsx
+++ src/navegacao/bottom/InferiorNav.tsx
@@ -1,20 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { PrincipalNavegacao, SecundariaNavegacao, TerceiraNavegacao } from "../stack/SuperiorNav.tsx"; 
 
 import COR from "../../assets/CSS/COR";
 import inicio from '../../assets/icons/inicio.png';
-import logado from '../../assets/icons/logado.png';
-import deslogado from '../../assets/icons/deslogado.png';
+import logadoIMG from '../../assets/icons/logado.png';
+import deslogadoIMG from '../../assets/icons/deslogado.png';
 import chave from '../../assets/icons/chave.png';
 import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const InferiorNav = ({navigation}) => {
+export default function InferiorNav ({navigation}){
+
+  const [varTempo,setVarTempo] = useState(0)
+
+  const timer = setTimeout(() => {
+    setVarTempo(varTempo+1)
+  }, 1000);
+  
+
+  const [logado,setLogado] = useState(0);
+
+
   useEffect(()=>{
-    
-  },[])
+    async function PegaLogin(){
+      const tipoDeLogin = await AsyncStorage.getItem('logado'); 
+      if(tipoDeLogin == undefined || tipoDeLogin == null || tipoDeLogin == "false"){
+        if(logado!=0){
+            setLogado(0)
+        }
+      }
+      else{
+        if(logado!=1){
+          setLogado(1)
+      }
+      }
+    }
+   PegaLogin();
+   if(varTempo>9){
+    setVarTempo(0)
+   }
+   return () => clearTimeout(timer);
+  },[varTempo]) 
 
     return (
         <Tab.Navigator screenOptions={{
@@ -32,7 +61,7 @@ const InferiorNav = ({navigation}) => {
           tabBarLabel: 'Login',tabBarIcon: () => {
             return (<Image 
                 style={{ width: 30, height: 30 }}
-                source={deslogado}/>)}}}
+                source={logado == 0? deslogadoIMG : logadoIMG}/>)}}}
                 />
           <Tab.Screen name="Chaves" component={TerceiraNavegacao} options={{
           tabBarLabel: 'Chaves',tabBarIcon: () => {
@@ -42,5 +71,3 @@ const InferiorNav = ({navigation}) => {
           </Tab.Navigator>
       );
 };
-
-export default InferiorNav;
